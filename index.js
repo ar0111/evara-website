787bb7ea4388fe4a2f594d2406ac4fbe337d4651
Arafat-Rahman <arafat.cuet99@gmail.com>
2023-08-12 13:05:47 +0600

    Add Functionality in Product Section

--- index.js
+++ index.js
@@ -22,3 +22,25 @@ var swipercategories = new Swiper(".mySwiper", {
         },
     },
 });
+
+// Product Tabs
+
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[content]');
+
+tabs.forEach((tab)=>{
+  tab.addEventListener('click', ()=>{
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) =>{
+      tabContent.classList.remove('active-tab');
+    });
+
+    target.classList.add('active-tab');
+
+    tabs.forEach((tab) =>{
+      tab.classList.remove('active-tab');
+    });
+
+    tab.classList.add('active-tab');
+  });
+});
